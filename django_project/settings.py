dd9e0b0b3b4623203cf8218c7fb0179436ba44ee
erkirandahal <erkirandahal@gmail.com>
2020-11-11 16:11:45 +0545

    Postgres COnfigured

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'db_pis',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '5433',
+
     }
+
 }
 
 
